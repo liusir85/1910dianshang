b813aca27d8b7ed0e1812d7e840383e8c12d226e
liusir85 <893518673@qq.com>
2020-08-12 02:02:33 +0000

    first commit

--- resources/views/Index/product_list.blade.php
+++ resources/views/Index/product_list.blade.php
@@ -20,16 +20,6 @@
             <div class="row">
                 <div class="col s6">
                     <div class="content">
-                        <img src="img/product-new1.png" alt="">
-                        <h6><a href="">Fashion Men's</a></h6>
-                        <div class="price">
-                            $20 <span>$28</span>
-                        </div>
-                        <button class="btn button-default">ADD TO CART</button>
-                    </div>
-                </div>
-                <div class="col s6">
-                    <div class="content">
                         <img src="img/product-new2.png" alt="">
                         <h6><a href="">Fashion Men's</a></h6>
                         <div class="price">
